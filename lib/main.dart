c5819311ba0e55a2674478dcdb0c4b41c9aa70bd
andreybelous <ab@aqsi.ru>
2018-08-02 22:26:07 +0300

    added button and replaced column with list view

--- lib/main.dart
+++ lib/main.dart
@@ -16,45 +16,96 @@ class HomePage extends State<MainPage> {
       theme: ThemeData(
         hintColor: Colors.grey[300],
         fontFamily: "MonsteratRegular",
+        textSelectionColor: Colors.white,
         inputDecorationTheme: InputDecorationTheme(
           border: UnderlineInputBorder(),
         ),
       ),
       home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/background.png"),
-                  fit: BoxFit.fill,
-                ),
-              ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/background.png"),
+              fit: BoxFit.fill,
             ),
-            Center(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 100.0),
-                    child: Text(
-                      "Sign up",
-                      style: TextStyle(fontSize: 32.0, color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    width: 240.0,
-                    padding: EdgeInsets.only(top: 40.0),
-                    child: TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: "Name",
+          ),
+          child: ListView(
+            children: <Widget>[
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 60.0),
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(fontSize: 32.0, color: Colors.white),
                       ),
                     ),
-                  ),
-                ],
+                    Container(
+                      width: 200.0,
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "MonsteratRegular",
+                          fontSize: 16.0,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 200.0,
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "MonsteratRegular",
+                          fontSize: 16.0,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 200.0,
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "MonsteratRegular",
+                          fontSize: 16.0,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: Container(
+                        width: 200.0,
+                        height: 40.0,
+                        child: RaisedButton(
+                          textTheme: ButtonTextTheme.accent,
+                          onPressed: () => debugPrint(""),
+                          child: Text("Sign Up"),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
